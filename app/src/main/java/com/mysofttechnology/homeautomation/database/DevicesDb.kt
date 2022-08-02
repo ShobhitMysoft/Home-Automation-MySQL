ce7e91651c6e6293f5f9dcb11882c178fcf793ce
ShobhitMysoft <shobhit.mysoft@gmail.com>
2022-08-02 17:24:51 +0530

    migration to version 2 is complete

--- app/src/main/java/com/mysofttechnology/homeautomation/database/DevicesDb.kt
+++ app/src/main/java/com/mysofttechnology/homeautomation/database/DevicesDb.kt
@@ -4,13 +4,30 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Device::class], version = 1, exportSchema = false)
-abstract class DevicesDb: RoomDatabase() {
+@Database(entities = [Device::class], version = 2, exportSchema = false)
+abstract class DevicesDb : RoomDatabase() {
 
     abstract fun deviceDao(): DeviceDao
 
     companion object {
+
+        private val migration_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "ALTER TABLE devices ADD COLUMN switchCount TEXT NOT NULL DEFAULT(0)")
+                database.execSQL(
+                    "ALTER TABLE devices ADD COLUMN s6Name TEXT NOT NULL DEFAULT(0)")
+                database.execSQL(
+                    "ALTER TABLE devices ADD COLUMN s6Icon INTEGER NOT NULL DEFAULT(0)")
+                database.execSQL(
+                    "ALTER TABLE devices ADD COLUMN s6State INTEGER NOT NULL DEFAULT(0)")
+            }
+
+        }
+
         @Volatile
         private var INSTANCE: DevicesDb? = null
 
@@ -24,7 +41,9 @@ abstract class DevicesDb: RoomDatabase() {
                     context.applicationContext,
                     DevicesDb::class.java,
                     "devices"
-                ).build()
+                )
+                    .addMigrations(migration_1_2)
+                    .build()
                 INSTANCE = instance
                 return instance
             }
